293d6da7bc4986f0beca1008f397193bbfaf3d20
Jason Corum <jason.corum@gmail.com>
2019-05-10 10:22:09 -0700

    Fixes tests for Instantiations

--- backend/spec/factories.rb
+++ backend/spec/factories.rb
@@ -1,20 +1,16 @@
-require_relative '../../schemas/digital_object_ext'
-require_relative '../../schemas/instantiation'
+require 'factory_bot'
 
 FactoryBot.define do
-  def JSONModel(key)
-    JSONModel::JSONModel(key)
-  end
   # JSON Models:
 
-  factory :json_digital_object_with_instantiation, class: JSONModel(:digital_object) do
+  factory :json_digital_object_with_instantiations, class: JSONModel(:digital_object) do
     title { "Digital Object #{generate(:generic_title)}" }
     language { generate(:archival_object_language) }
     digital_object_id { generate(:alphanumstr) }
     extents { [build(:json_extent)] }
     file_versions { few_or_none(:json_file_version) }
     dates { few_or_none(:json_date) }
-    instantiations { [ build(:json_instantiation) ] }
+    instantiations { [ build(:json_instantiation), build(:json_instantiation) ] }
   end
 
   factory :json_instantiation, class: JSONModel(:instantiation) do
--- backend/spec/model_instantiation_spec.rb
+++ backend/spec/model_instantiation_spec.rb
@@ -1,13 +1,18 @@
-require 'rspec'
+require 'spec_helper'
 require_relative 'factories'
-require_relative '../model/instantiation'
-require_relative '../../../../backend/spec/spec_helper'
-require_relative '../../../../backend/spec/spec_slugs_helper'
 
-describe 'Instantiation' do
-  it "Digital Objects can be created with an Instantiation" do
-    json = build(:json_digital_object_with_instantiation)
-    digital_object = DigitalObject.create_from_json(json, :repo_id => $repo_id)
-    expect(digital_object.instantiations.length).to eq(1)
+describe 'Instantiations' do
+  let(:json) { build(:json_digital_object_with_instantiations, digital_object_id: '123') }
+  let(:digital_object) { DigitalObject.create_from_json(json) }
+
+  it "Can be created with Digital Objects" do
+    expect(digital_object.instantiation.length).to eq(2)
+  end
+
+  it "Can have all the expected attributes populated" do
+    instantiation = digital_object.instantiation.sample
+    instantiation.keys.each do |key|
+      expect(instantiation[key]).to_not eq(nil)
+    end
   end
 end
