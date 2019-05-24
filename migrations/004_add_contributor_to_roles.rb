1d736f5066fb74facaf7dd79c3f351f9855cc7e5
Jason Corum <jason.corum@gmail.com>
2019-05-24 14:47:46 -0700

    Adds Contributor to AgentLink role enum

---
+++ migrations/004_add_contributor_to_roles.rb
@@ -0,0 +1,10 @@
+require_relative 'utils'
+
+Sequel.migration do
+
+  up do
+    obj = self[:enumeration].where(name: "linked_agent_role").first
+    add_editable_enum_values(obj, [ "Contributor" ])
+  end
+
+end
